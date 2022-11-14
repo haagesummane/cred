27580cc729b2cc82e218b67f6b3438eebe96d35f
Manu Hegde <manuhegdev@gmail.com>
2022-11-14 17:52:06 +0530

    instructions list implemented

--- opcode_executor/model/instructions/instructions_list.py
+++ opcode_executor/model/instructions/instructions_list.py
@@ -1,1 +1,35 @@
+from dataclasses import dataclass
+from typing import Set, List, Optional, Dict
+import logging
+
+from opcode_executor.model.instructions.instructions_parent import InstructionsParent
+
+
+@dataclass
+class IllegalInstruction:
+    FAIL = 1
+    SKIP = 2
+
+
 class InstructionsList:
+    def __init__(self, allowed_instructions: Dict[str, InstructionsParent], sep: Optional[str] = ' ',
+                 illegal_inst: IllegalInstruction = IllegalInstruction.FAIL):
+        self.allowed_inst = allowed_instructions
+        self.separator = sep
+        self.on_illegal_instruction = illegal_inst
+        self.parsed_instructions = None
+
+    def parse_inst(self, inst_str: str) -> InstructionsParent:
+        args = list(map(lambda s: s.strip(), inst_str.split(self.separator)))
+        inst = args[0].upper()
+        if inst not in self.allowed_inst:
+            if self.on_illegal_instruction == IllegalInstruction.FAIL:
+                raise Exception("Instruction not found in allowed instruction set!")
+            if self.on_illegal_instruction == IllegalInstruction.SKIP:
+                logging.warning(f'skipping illegal instruction {inst} => {inst_str}')
+
+        return self.allowed_inst[inst](args[1:])
+
+    def parse_instructions(self, instructions: List[str]) -> List[InstructionsParent]:
+        self.parsed_instructions = list(map(self.parse_inst, instructions))
+        return self.parsed_instructions
--- opcode_executor/model/instructions/instructions_parent.py
+++ opcode_executor/model/instructions/instructions_parent.py
@@ -2,29 +2,21 @@ from typing import List
 
 
 class InstructionsParent:
-    def __init__(self, ins_str:str):
-        self.instruction_str = ins_str
-
-        self.separator:str = None
-        self.instruction = None
+    def __init__(self, params: List):
+        self.params = params
         self.num_inp_params = None
-        self.instr_name = None # ideally this should be equal to self.__class__.__name__
+        self.instr_name = None  # ideally this should be equal to self.__class__.__name__
 
-    def parse(self):
-        inputs = self.instruction_str.split(self.separator)
-        if inputs[0].strip()!=self.instr_name:
-
+    def config_checks(self):
+        if self.instr_name != self.instr_name.upper():
+            raise Exception('Developer WARNING: All instruction names should be in upper case!')
 
     @property
-    def instruction(self,ins_str:str):
-        self.instruction=self.parse(ins_str)
+    def instruction(self, ins_str: str):
+        self.instruction = self.parse(ins_str)
 
-
-    def validate(self, params:List):
+    def validate(self, params: List):
         raise Exception("instruction_parent.validate must be implemented!")
 
-    def execute(self, params:List):
+    def execute(self, params: List):
         raise Exception("instruction_parent.execute must be implemented!")
-
-
-
